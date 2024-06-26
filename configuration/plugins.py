7d8458f529d0ff2ad34399a9ff3a8bb20e463eb8
Jakub Krkoška <jakub.krkoska@invia.sk>
2024-06-26 10:52:19 +0200

    feat: added ui vizualization plugin

--- configuration/plugins.py
+++ configuration/plugins.py
@@ -4,7 +4,7 @@
 # To learn how to build images with your required plugins
 # See https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
 
-PLUGINS = ["netbox_secrets"]
+PLUGINS = ["netbox_secrets", "nextbox_ui_plugin"]
 
 # PLUGINS_CONFIG = {
 #   "netbox_bgp": {
